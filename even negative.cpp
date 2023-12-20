c83852fabd58483b8786f9bed0bc3f758e30179c
Aivar <110456412+Ayvar22@users.noreply.github.com>
2023-12-21 00:50:05 +0600

    b

---
+++ even negative.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,a[100][100],sum=0,count=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n;j++){
+            if(a[i][j]<0 and a[i][j]%2==0){
+            sum+=a[i][j];
+            count++;
+            }
+        }
+    }
+    cout<<count<<" "<<sum;
+}
